280c62e7a91117621222d9f1de22b0390d3659dd
DatCodeMania <datcodemania@users.noreply.github.com>
2024-07-02 17:09:32 +0700

    Expand input options, beautify debugging output, add duration calculator.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -17,7 +18,44 @@ def add_session():
 @app.route('/submit', methods=['POST'])
 def submit():
     selected_options = request.form.getlist('muscle_groups_trained')
-    return f'Selected options: {", ".join(selected_options)}'
+    time_started = request.form.get('time_started')
+    time_stopped = request.form.get('time_stopped')
+    rpe = request.form.get('rpe')
+    satisfaction_rating = request.form.get('satisfaction_rating')
+    motivation_rating = request.form.get('motivation_rating')
+    notes = request.form.get('notes')
+    exercises_done = []
+    exercise_names = request.form.getlist('exercise_name[]')
+    sets_done = request.form.getlist('sets_done[]')
+    reps_done = request.form.getlist('reps_done[]')
+
+    current_rep_index = 0
+    for i in range(len(exercise_names)):
+        sets = int(sets_done[i])
+        reps = reps_done[current_rep_index:current_rep_index + sets]
+        current_rep_index += sets
+        exercises_done.append({
+            'exercise_name': exercise_names[i],
+            'sets_done': sets,
+            'reps_done': reps
+        })
+
+    time_format = "%H:%M"
+    start_time = datetime.strptime(time_started, time_format)
+    stop_time = datetime.strptime(time_stopped, time_format)
+    duration = stop_time - start_time
+    workout_duration = duration.total_seconds() / 60
+    return f'''
+        Selected options: {", ".join(selected_options)}<br>
+        Time started: {time_started}<br>
+        Time stopped: {time_stopped}<br>
+        Dura: {workout_duration}<br>
+        RPE: {rpe}<br>
+        Satisfaction rating: {satisfaction_rating}<br>
+        Motivation rating: {motivation_rating}<br>
+        Notes: {notes}<br>
+        Exercises done: {exercises_done}
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
